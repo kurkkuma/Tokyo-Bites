9a00959ba57e0d637d5b53fc193bb68aaa09a3fb
Alina Vetchanina <vetchanina.alina@gmail.com>
2023-07-10 16:00:06 +0300

    Adaptive layout (About)

--- src/components/about/reviews/Reviews.tsx
+++ src/components/about/reviews/Reviews.tsx
@@ -6,13 +6,21 @@ function Reviews() {
     <div className="reviews">
       <h1 className="title">feedback from our customers</h1>
       <div className="container">
-        <img src="/images/icons/arrow-left.png" alt="arrow" />
+        <img
+          className="arrow-left"
+          src="/images/icons/arrow-left.png"
+          alt="arrow"
+        />
         <div className="reviews-container">
           <ReviewCard />
           <ReviewCard />
           <ReviewCard />
         </div>
-        <img src="/images/icons/arrow-right.png" alt="arrow" />
+        <img
+          className="arrow-right"
+          src="/images/icons/arrow-right.png"
+          alt="arrow"
+        />
       </div>
     </div>
   );
