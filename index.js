e5d2cef497c8312e82d28b173b73a4d53bbfc68e
Alina Vetchanina <vetchanina.alina@gmail.com>
2023-09-01 17:49:00 +0200

    get/post reviews

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cors = require("cors");
 //MODELS
 const User = require("./models/user");
 const Product = require("./models/product");
+const Review = require("./models/review");
 
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -22,46 +23,6 @@ mongoose
   .catch((err) => console.log(err));
 
 // =========================================================================
-
-// const initialData = [
-//   {
-//     url: "/images/products/913b1040b4b6c971cc5fda13d828661e.jpg",
-//     name: "Kani",
-//     price: 2,
-//     description: "crab meat, sushi rice, cucumber, nori, wasabi",
-//     composition:
-//       "rice for sushi (polished rice, not steamed, rice vinegar, alcohol vinegar, sugar, water, salt), boiled and frozen crab meat, cucumber, wasabi (horseradish, mustard, corn starch, acidity regulators: citric acid E330, ascorbic acid E300, dyes: blue brilliant E133, tartrazine E102), dried algae of the genus Porphyra.",
-//     category: "gunkans",
-//     kcal: 42,
-//     weight: 30,
-//     proteins: 2.8,
-//     fats: 0.4,
-//     carbohydrates: 6.9,
-//     tags: ["crab meat"],
-//   },
-//   {
-//     url: "/images/products/4ac1b1c340eee30587fbc3d6dbea31d8.jpg",
-//     name: "Persona grata",
-//     price: 5,
-//     description:
-//       "smoked eel, cheese, avocado, masago, sushi rice, sesame, tempura, spicy sauce. Served with citrus spicy sauce (4 pcs.)",
-//     composition:
-//       "rice for sushi (polished rice not steamed, rice vinegar, alcohol vinegar, sugar, water, salt), minced meat (curd cheese (cottage cheese (normalized pasteurized cow's milk, bacterial starter, milk-clotting enzyme of microbial origin), modified starch thickener, Vidogum stabilizer ( locust bean gum and guar gum), acidity regulator citric acid, table salt, drinking water), Philadelphia sauce (curd cheese (cottage cheese (normalized pasteurized cow's milk, bacterial starter culture, milk-clotting enzyme of microbial origin)",
-//     kcal: 568,
-//     weight: 165,
-//     proteins: 11.3,
-//     fats: 40.7,
-//     carbohydrates: 39.0,
-//     tags: ["eel", "avocado"],
-//   },
-// ];
-
-// try {
-//   Product.insertMany(initialData);
-//   console.log("Initial data added");
-// } catch (error) {
-//   console.error("Error adding initial data:", error);
-// }
 
 app.post("/add-user", async (req, res) => {
   const { name, phone, address } = req.body;
@@ -100,6 +61,28 @@ app.get("/products", async (req, res) => {
     res.json(products);
   } catch (error) {
     console.error("Error fetching products:", error);
+  }
+});
+
+app.post("/add-review", async (req, res) => {
+  const { userName, date, stars, text } = req.body;
+  try {
+    const newReview = new Review({ userName, date, stars, text });
+    await newReview.save();
+
+    res.status(200).json(newReview);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Failed to save user" });
+  }
+});
+
+app.get("/reviews", async (req, res) => {
+  try {
+    const reviews = await Review.find();
+    res.json(reviews);
+  } catch (error) {
+    console.error("Error fetching reviews:", error);
   }
 });
 //запуск сервера
---
+++ models/review.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const reviewSchema = new Schema({
+  userName: {
+    type: String,
+    require: true,
+  },
+  date: {
+    type: String,
+    require: true,
+  },
+  stars: {
+    type: Number,
+    require: true,
+  },
+  text: {
+    type: String,
+    require: true,
+  },
+  photo: {
+    type: String,
+    require: false,
+  },
+});
+
+const Review = mongoose.model("Review", reviewSchema, "reviews");
+
+module.exports = Review;
