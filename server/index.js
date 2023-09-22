d9ad0fb8818bdf1f6061024f7363665dbde32c96
Alina Vetchanina <vetchanina.alina@gmail.com>
2023-09-22 18:17:20 +0200

    fixing routing (3)

--- server/index.js
+++ server/index.js
@@ -25,8 +25,10 @@ mongoose
 
 // =========================================================================
 
+app.use(express.static(path.join(__dirname, "../client")));
+
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/index.html"));
+  res.sendFile(path.join(__dirname, "../client", "index.html"));
 });
 
 app.post("/add-user", async (req, res) => {
