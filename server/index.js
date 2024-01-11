3b479f036d1f557b249f17d758faf61631e1ffac
Alina Vetchanina <vetchanina.alina@gmail.com>
2024-01-11 11:46:56 +0100

    changed server code

--- server/index.js
+++ server/index.js
@@ -20,10 +20,10 @@ mongoose
 
 // =========================================================================
 
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 // =========================================================================
 app.get("/", (req, res) => {
