1f067e22befa5d0c05f75b363aea34c733a4b535
Alina Vetchanina <vetchanina.alina@gmail.com>
2024-01-11 12:34:59 +0100

    changed server files (3)

--- server/index.js
+++ server/index.js
@@ -20,11 +20,11 @@ mongoose
 
 // =========================================================================
 
-app.use(express.static(path.join(__dirname, "../client/dist")));
+// app.use(express.static(path.join(__dirname, "../client/dist")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
+// });
 // =========================================================================
 app.get("/", (req, res) => {
   res.send("Tokyo Bites server");
