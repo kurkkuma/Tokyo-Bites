04caee44a9a4cdc4c73d9cdc6c4b3c9965c203b6
Alina Vetchanina <vetchanina.alina@gmail.com>
2024-01-16 12:28:53 +0100

    fixed reviews slider

--- client/src/components/about/reviews/Reviews.tsx
+++ client/src/components/about/reviews/Reviews.tsx
@@ -1,14 +1,15 @@
-import { useState, useRef, useEffect } from "react";
+import { useState, useEffect } from "react";
 import ReviewCard from "./review-card/ReviewCard";
 import { useGetReviewsQuery } from "../../../store/api/reviewsApi";
 import { useAppSelector } from "../../../store/hooks";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
 
 function Reviews() {
   const user = useAppSelector((state) => state.user.user);
   const { data = [] } = useGetReviewsQuery({});
-  const [containerOffset, setContainerOffset] = useState<number>(0);
-  const [windowWidth, setWindowWidth] = useState<number>();
-  const windowRef = useRef<HTMLDivElement | null>();
+  // const [itemsPerGroup, setItemsPerGroup] = useState(3);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
   const sortedData = [...data].sort((a, b) => {
     const starsComparison = b.stars - a.stars;
@@ -25,91 +26,69 @@ function Reviews() {
     }
   });
 
-  const groupedData = [];
-  for (let i = 0; i < sortedData.length; i += 3) {
-    groupedData.push(sortedData.slice(i, i + 3));
-  }
+  const updateGroupedData = () => {
+    const newGroupedData = [];
+    const itemsPerGroup = windowWidth > 700 ? 3 : windowWidth > 470 ? 2 : 1;
+
+    for (let i = 0; i < sortedData.length; i += itemsPerGroup) {
+      newGroupedData.push(sortedData.slice(i, i + itemsPerGroup));
+    }
+
+    return newGroupedData;
+  };
 
   useEffect(() => {
-    const allowedWidths = [300, 600, 700, 900];
-    const updateWindowWidth = () => {
-      if (windowRef.current) {
-        const currentWidth = windowRef.current.offsetWidth;
-        if (allowedWidths.includes(currentWidth)) {
-          setContainerOffset(0);
-        }
-        setWindowWidth(currentWidth);
-      }
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
     };
-    updateWindowWidth();
 
-    window.addEventListener("resize", updateWindowWidth);
+    window.addEventListener("resize", handleResize);
+
     return () => {
-      window.removeEventListener("resize", updateWindowWidth);
+      window.removeEventListener("resize", handleResize);
     };
   }, []);
-
+  const groupedData = updateGroupedData();
   return (
     <div className="reviews">
-      <h1 className="title">feedback from our customers</h1>
-      <div className="container">
-        <img
-          onClick={() => {
-            if (windowWidth) {
-              containerOffset === 0
-                ? ""
-                : setContainerOffset(containerOffset + windowWidth);
+      <div className="reviews-container">
+        <h1 className="title">feedback from our customers</h1>
+
+        {groupedData.length > 0 && (
+          <Carousel
+            width={
+              windowWidth > 900
+                ? 900
+                : windowWidth > 700
+                ? 700
+                : windowWidth > 470
+                ? 500
+                : 250
             }
-          }}
-          className="arrow-left"
-          src="/images/icons/arrow-left.png"
-          alt="arrow"
-        />
-        <div className="reviews-container">
-          <div className="window">
-            <div
-              ref={(element) => {
-                if (element) {
-                  windowRef.current = element;
-                }
-              }}
-              style={{ transform: `translateX(${containerOffset}px)` }}
-              className="groups-container"
-            >
-              {groupedData.map((group, groupIndex: number) => {
-                return (
-                  <div key={groupIndex} className="reviews-group">
-                    {group.map((item) => {
-                      return (
-                        <ReviewCard
-                          key={item._id}
-                          id={item._id}
-                          userName={item.userName}
-                          date={item.date}
-                          stars={item.stars}
-                          text={item.text}
-                          deleteSign={user._id === item.userId ? true : false}
-                        />
-                      );
-                    })}
-                  </div>
-                );
-              })}
-            </div>
-          </div>
-        </div>
-        <img
-          onClick={() => {
-            if (windowWidth) {
-              containerOffset === -((groupedData.length - 1) * windowWidth)
-                ? ""
-                : setContainerOffset(containerOffset - windowWidth);
-            }
-          }}
-          className="arrow-right"
-          src="/images/icons/arrow-right.png"
-          alt="arrow"
-        />
+            showThumbs={false}
+            showStatus={false}
+          >
+            {groupedData.map((group: any, groupIndex: number) => {
+              return (
+                <div key={groupIndex} className="reviews-group">
+                  {group.map((item: any) => {
+                    return (
+                      <ReviewCard
+                        key={item._id}
+                        id={item._id}
+                        userName={item.userName}
+                        date={item.date}
+                        stars={item.stars}
+                        text={item.text}
+                        deleteSign={user._id === item.userId ? true : false}
+                      />
+                    );
+                  })}
+                </div>
+              );
+            })}
+          </Carousel>
+        )}
       </div>
     </div>
   );
