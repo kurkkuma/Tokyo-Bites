cdc7637a5fe331bc5ec8416b4e56bd9b7a8afad2
Alina Vetchanina <vetchanina.alina@gmail.com>
2024-01-21 10:32:07 +0100

    changed base url

--- client/src/store/api/productsApi.ts
+++ client/src/store/api/productsApi.ts
@@ -3,8 +3,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const productsApi = createApi({
   reducerPath: "productsApi",
   baseQuery: fetchBaseQuery({
-    // baseUrl: "https://tokyo-bites-api.onrender.com/",
-    baseUrl: "http://localhost:8080/",
+    baseUrl: "https://tokyo-bites-api.onrender.com/",
+    // baseUrl: "http://localhost:8080/",
   }),
   endpoints: (builder) => ({
     getProducts: builder.query({
