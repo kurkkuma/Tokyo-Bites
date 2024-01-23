4db49f6437303c0d1be39e38084771dd66ae50cf
Alina Vetchanina <vetchanina.alina@gmail.com>
2024-01-23 16:18:01 +0100

    small changes

--- server/index.js
+++ server/index.js
@@ -3,7 +3,6 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 8080;
